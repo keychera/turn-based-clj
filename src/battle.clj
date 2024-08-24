005c0a391f624d7c9320f5131dd37e614c6cfd0c
keychera <kyogatama@gmail.com>
2024-08-24 18:41:46 +0900

    keyevent

--- src/battle.clj
+++ src/battle.clj
@@ -1,7 +1,7 @@
 (ns battle
   (:require [clojure.core.match :refer [match]]
             [clojure.string :as str]
-
+            [hiccup2.core :refer [html]]
             [selmer.parser :refer [render-file]]))
 
 
@@ -15,11 +15,18 @@
   (render-file "battle.html" {:status battle-state
                               :tick   @tick}))
 
+(defn post-battle-state [_]
+  (swap! tick inc)
+  (str (html [:div#state {:hx-post "/state" :hx-trigger "updown-event from:body"}
+              [:p "Tick " @tick]
+              [:p (str battle-state)]])))
+
 (defn router [req]
   (let [paths (some-> (:uri req) (str/split #"/") rest vec)
         verb  (:request-method req)]
     (match [verb paths]
-      [:get []] {:body (battle-html req)}
+      [:get []]         {:body (battle-html req)}
+      [:post ["state"]] {:body (post-battle-state req)}
       :else {:status 404
              :body   "not found"})))
 
