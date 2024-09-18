6b9338e08b779984c238fa5e4d50fe4d62f2c160
keychera <kyogatama@gmail.com>
2024-09-18 23:43:41 +0900

    group moments per turn

--- src/battle_web.clj
+++ src/battle_web.clj
@@ -1,36 +1,53 @@
 (ns battle-web
   (:require [clojure.core.match :refer [match]]
             [clojure.string :as str]
-            [common :refer [htmx?]]
+            [common :refer [htmx? query->vec]]
             [hiccup2.core :refer [html]]
             [selmer.parser :refer [render-file]]
             [timeline :refer [history initial-state reduce-timeline turn]]))
 
-(defn timeline-html [turn]
-  (let [timeline (reduce-timeline initial-state @history turn)]
-    (str (html [:div {:id "timeline" :hx-push-url "true" :hx-target "#timeline"}
-                [:div {:hx-get (str "/timeline/" (inc turn)) :hx-trigger "keyup[keyCode==40] from:body"}]
-                (when (> turn 0)
-                  [:div {:hx-get (str "/timeline/" (dec turn)) :hx-trigger "keyup[keyCode==38] from:body"}])
-                [:p "Turn " turn]
-                [:ol (->> timeline
-                          (map (fn [{:state/keys [entities desc]}]
-                                 [:li [:p (str entities)] [:p [:b (str desc)]]])))]]))))
+(defn timeline-html
+  ([turn] (timeline-html turn 0))
+  ([turn moment]
+   (let [timeline (reduce-timeline initial-state @history turn)
+         [current-turn & prev-turns] (->> timeline rseq (group-by :state/turn))]
+     (str (html [:div {:id "timeline" :hx-push-url "true" :hx-target "#timeline"}
+                 [:div {:hx-get (str "/timeline/" (inc turn)) :hx-trigger "keyup[keyCode==40] from:body"}]
+                 (when (> turn 0)
+                   [:div {:hx-get (str "/timeline/" (dec turn)) :hx-trigger "keyup[keyCode==38] from:body"}])
+                 [:p "Current turn " turn]
+                 (->> prev-turns
+                      (mapv (fn [[turn moments]]
+                              (let [last-moment (peek moments)
+                                    {:state/keys [entities]} last-moment]
+                                [:div
+                                 [:p (str "Turn #" (or turn 0))]
+                                 [:p (str entities)]
+                                 [:ol (->> moments (map (fn [{:state/keys [desc]}] [:li  [:p [:b (str desc)]]])))]])))
+                      rseq)
+                 (let [[turn moments] current-turn
+                       last-moment (peek moments)
+                       {:state/keys [entities]} last-moment]
+                   [:div
+                    [:p (str "Turn #" (or turn 0))]
+                    [:p (str entities)]
+                    [:ol (->> moments (map (fn [{:state/keys [desc]}] [:li  [:p [:b (str desc)]]])))]])])))))
 
 (defn battle-html [turn]
   (render-file "battle.html" {:timeline-html (timeline-html turn)}))
 
 (defn get-timeline [req turn]
-  (let [turn (Integer/valueOf turn)]
+  (let [turn (Integer/valueOf turn)
+        moment (or (some-> (:query-string req) query->vec :moment) 1)]
     (if (htmx? req)
-      (timeline-html turn)
+      (timeline-html turn moment)
       (battle-html turn))))
 
 (defn router [req]
   (let [paths (some-> (:uri req) (str/split #"/") rest vec)
         verb  (:request-method req)]
     (match [verb paths]
-      [:get  []]           {:body (battle-html 0)}
+      [:get  []]                {:body (battle-html 0)}
       [:get  ["timeline" turn]] {:body (get-timeline req turn)}
       :else {:status 404
              :body   "not found"})))
@@ -44,6 +61,7 @@
   (add-tap #(def last-tap %))
   (add-tap println)
   last-tap
+
 
   (def stop-all-fn
     (let [watch-dir "src" url "localhost" port 4242
