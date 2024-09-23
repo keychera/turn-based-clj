43ac6a5a7b4afa37e8bbe40c42e702fa48d3ab82
keychera <kyogatama@gmail.com>
2024-09-23 21:07:20 +0900

    preliminary triplestore impl

--- src/engine/timeline.clj
+++ src/engine/timeline.clj
@@ -1,23 +1,43 @@
 (ns engine.timeline
-  (:require [com.rpl.specter :as sp :refer [select setval]]))
+  (:require [engine.triplestore :refer [gen-dynamic-eid get-entity q-one
+                                        transform-entity]]))
+
+(def battle-data
+  #:battle-data
+   {:num-moment-per-turn 2
+    :actors [:actor/hilda :actor/aluxes]
+    :history-atom
+    (atom [#:moment{:whose  :actor/hilda
+                    :action '(-> :actor/hilda (poison :actor/aluxes))}
+           #:moment{:whose  :actor/aluxes
+                    :action '(-> :actor/aluxes (basic-attack :actor/hilda))}
+
+           #:moment{:whose  :actor/hilda
+                    :action '(-> :actor/hilda (basic-attack :actor/aluxes))}
+           #:moment{:whose  :actor/aluxes
+                    :action '(-> :actor/aluxes (basic-attack :actor/hilda))}
+
+           #:moment{:whose  :actor/hilda
+                    :action '(-> :actor/hilda (poison :actor/aluxes))}
+           #:moment{:whose  :actor/aluxes
+                    :action '(-> :actor/aluxes (basic-attack :actor/hilda))}])})
+
+(def initial-state-triple
+  [[:info/state :state/turn 0]
+   [:info/state :state/desc "battle begins"]
+   [:info/state :state/actors [:actor/hilda :actor/aluxes]]
+   [:actor/hilda :attr/hp 560]
+   [:actor/hilda :attr/mp 200]
+   [:actor/aluxes :attr/hp 800]
+   [:actor/aluxes :attr/mp 10]])
+
 
 ;; Engine
-
-(defn entities->effect-data [entities]
-  (->> entities
-       (mapcat (fn [[affected attr]]
-                 (->> (:attr/effect attr)
-                      (map (fn [[effect-name effect-data]]
-                             (assoc effect-data
-                                    :effect-data/effect-name effect-name
-                                    :effect-data/affected affected))))))))
-
-(defn reduce-effect-duration [{:effect-data/keys [duration affected effect-name]} state]
+(defn reduce-effect-duration [state {:effect-data/keys [effect-id duration]}]
   (let [new-duration (dec duration)]
-    (cond
-      (= duration -1) state
-      (= new-duration 0) (->> state (setval [:state/entities affected :attr/effect effect-name] sp/NONE))
-      :else (->> state (setval [:state/entities affected :attr/effect effect-name :effect-data/duration] new-duration)))))
+    (cond (= duration -1) state
+          (= new-duration 0) (transform-entity state effect-id {:effect-data/duration nil})
+          :else (transform-entity state effect-id {:effect-data/duration new-duration}))))
 
 (defmulti unleash-effect :effect-data/effect-name)
 
@@ -25,12 +45,13 @@
 
 (defn reduce-effects [original-timeline event]
   (let [state (peek original-timeline)
-        all-affected (select [:state/entities sp/ALL (sp/selected? (fn [[_ attr]] (:attr/effect attr)))] state)
-        effects (entities->effect-data all-affected)]
-    (->> effects
-         (reduce (fn [timeline effect-data]
+        effects-id (d/q '[:find ?affected ?effect-id :where [?affected :attr/effect ?effect-id]] state)]
+    (->> effects-id
+         (map (fn [[affected effects-id]] [affected effects-id (get-entity state effects-id)]))
+         (reduce (fn [timeline [affected effect-id effect-data]]
                    (let [state (peek timeline)
-                         new-moment (unleash-effect (assoc effect-data :effect-data/event event :effect-data/state state))]
+                         extra-data #:effect-data{:affected affected :effect-id effect-id :event event :state state}
+                         new-moment (unleash-effect (merge effect-data extra-data))]
                      (cond-> timeline
                        (some? new-moment) (conj new-moment))))
                  original-timeline))))
@@ -39,6 +60,40 @@
   (require ns-symbol)
   (let [user-ns (create-ns ns-symbol)]
     (binding [*ns* user-ns] (clojure.core/eval form))))
+
+(defn basic-attack [actor target]
+  (fn [state]
+    (let [damage 50]
+      (-> state
+          (transform-entity target {:attr/hp #(- % damage)})
+          (transform-entity :info/state {:state/desc (str actor " attacks " target " for " damage " damage!")})))))
+
+(defn poison
+  ([actor target] (poison actor target #:effect-data{:duration 3}))
+  ([actor target {:effect-data/keys [duration]}]
+   (fn [state]
+     (let [manacost 30 debuff :debuff/poison
+           current-poison (q-one '[:find ?eid :in $ ?target ?name
+                                   :where [?target :attr/effect ?eid]
+                                   [?eid :effect-data/effect-name ?name]]
+                                 state target debuff)
+           poison-entity (or current-poison (gen-dynamic-eid state))]
+       (-> state
+           (transform-entity target {:attr/mp #(- % manacost)
+                                     :attr/effect poison-entity})
+           (transform-entity poison-entity #:effect-data{:effect-name debuff :source actor :duration duration})
+           (transform-entity :info/state {:state/desc (str actor " poisons " target " ! " target " is now poisoned!")}))))))
+
+(defmethod unleash-effect :debuff/poison
+  [{:effect-data/keys [affected event state] :as effect-data}]
+  (when (= event :event/on-turn-begins)
+    (let [affected-entity (get-entity state affected)
+          affected-hp (:attr/hp affected-entity)
+          damage (Math/floor (/ affected-hp 10))]
+      (-> state
+          (reduce-effect-duration effect-data)
+          (transform-entity affected {:attr/hp #(- % damage)})
+          (transform-entity :info/state {:state/desc (str affected " is poisoned! receives " damage " damage!")})))))
 
 (defn reduce-timeline
   ([model initial-state battle-data]
@@ -52,16 +107,18 @@
          (let [moments-per-turn (take num-moment-per-turn remaining-turns)
                remaining-turns (drop num-moment-per-turn remaining-turns)
                state (peek timeline)
-               new-timeline (conj [] (cond-> (update state :state/turn inc)
-                                       (empty? remaining-turns) (assoc :state/last-turn? true)))
+               new-timeline (conj [] (cond-> (transform-entity state :info/state {:state/turn inc})
+                                       (empty? remaining-turns) (conj [:info/state :state/last-turn? true])))
                new-timeline (reduce-effects new-timeline :event/on-turn-begins)
+
                new-timeline (loop [moment-timeline new-timeline
                                    [moment & remaining-moments] moments-per-turn]
                               (let [{:moment/keys [action]} moment
                                     moment-timeline (reduce-effects moment-timeline :event/on-moment-begins)
                                     alter (do-eval model action)
                                     state (peek moment-timeline)
-                                    moment-timeline (conj moment-timeline (alter state))
+                                    new-moment (alter state)
+                                    moment-timeline (conj moment-timeline new-moment)
                                     moment-timeline (reduce-effects moment-timeline :event/on-moment-ends)]
                                 (if (empty? remaining-moments)
                                   moment-timeline
@@ -74,4 +131,8 @@
 (comment
   (add-tap #(def last-tap %))
   (add-tap #(println %))
-  last-tap)
+  last-tap
+
+  basic-attack poison
+
+  (reduce-timeline 'engine.timeline initial-state-triple battle-data))
---
+++ src/engine/triplestore.clj
@@ -0,0 +1,37 @@
+(ns engine.triplestore
+  (:require [com.rpl.specter :as sp]
+            [matchete.core :as m]
+            [babashka.pods :as pods]))
+
+(pods/load-pod 'huahaiy/datalevin "0.9.10")
+(require '[pod.huahaiy.datalevin :as d])
+
+(defn gen-dynamic-eid [store]
+  (or (some->> (d/q '[:find ?entity :where [?entity _ _]] store)
+               (map first) (filter int?) sort last inc) 0))
+
+(defn transform-entity [original-store eid transform-map]
+  (->> transform-map
+       (reduce (fn [store [attrkey transformation]]
+                 (let [prev-attr (sp/select-one [sp/ALL #(m/match? [eid attrkey '_] %)] store)]
+                   (if (nil? prev-attr)
+                     (do
+                       (when (fn? transformation) (throw (IllegalStateException. (str "cannot transform value from non-exisiting attribute (" attrkey ")"))))
+                       (conj store [eid attrkey transformation]))
+                     (cond
+                       (nil? transformation) (sp/setval [sp/ALL #(m/match? [eid attrkey '_] %)] sp/NONE store)
+                       (fn? transformation) (sp/transform [sp/ALL #(m/match? [eid attrkey '_] %) sp/LAST] transformation store)
+                       :else (sp/setval [sp/ALL #(m/match? [eid attrkey '_] %)  sp/LAST] transformation store)))))
+               original-store)))
+
+(defn remove-entity [store eid]
+  (sp/setval [sp/ALL #(m/match? [eid '_ '_] %)] sp/NONE store))
+
+(defn get-entity [store eid]
+  (->> (d/q '[:find ?effect-data ?data :in $ ?eid
+              :where [?eid ?effect-data ?data]]
+            store eid)
+       (into {})))
+
+(defn q-one [query & inputs]
+  (-> (apply d/q query inputs) first first))
