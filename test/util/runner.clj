8d1bc238ac2bdd04b3046b46b0ad26502d4c2b99
keychera <kyogatama@gmail.com>
2024-09-24 21:04:28 +0900

    modified clojure.test/report

--- test/util/runner.clj
+++ test/util/runner.clj
@@ -1,6 +1,8 @@
 (ns util.runner
-  (:require [cognitect.test-runner :as tr]))
+  (:require [clojure.test]
+            [cognitect.test-runner :as tr]))
 
+;; relevant https://clojurians.slack.com/archives/CLX41ASCS/p1727176496828499
 (defn- do-test
   [{:keys [dirs nses patterns vars includes excludes]}]
   (let [adapted {:dir (when (seq dirs) (set dirs))
@@ -8,8 +10,17 @@
                  :namespace-regex (when (seq patterns) (map re-pattern patterns))
                  :var (when (seq vars) (set vars))
                  :include (when (seq includes) (set includes))
-                 :exclude (when (seq excludes) (set excludes))}]
-    (tr/test adapted)))
+                 :exclude (when (seq excludes) (set excludes))}
+        original-report clojure.test/report]
+    (with-redefs [clojure.test/report
+                  (fn [test-result]
+                    (let [modified-test-result
+                          (cond-> test-result
+                            (and (= :error (:type test-result)) (instance? Throwable (:actual test-result)))
+                            (update :actual #(-> % Throwable->map (dissoc :trace))))]
+                      (original-report modified-test-result)))]
+      (tr/test adapted))))
+
 
 (defn run-tests [opts]
   (let [test-results (do-test opts)
