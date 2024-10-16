15c82290643f0d9f7a7547835b5a957a3e116ead
keychera <kyogatama@gmail.com>
2024-10-17 00:52:33 +0900

    rules! first pass

--- src/engine2/timeline.clj
+++ src/engine2/timeline.clj
@@ -16,11 +16,13 @@
   #:moment.attr
    {:epoch    {:db/cardinality :db.cardinality/one}
     :desc     {:db/valueType :db.type/string}
-    :events   {:db/valueType   :db.type/ref
-               :db/cardinality :db.cardinality/many
+    :timing   {:db/cardinality :db.cardinality/one
+               :db/valueType :db.type/keyword}
+    :events   {:db/cardinality :db.cardinality/many
+               :db/valueType   :db.type/ref
                :db/isComponent true}
-    :entities {:db/valueType   :db.type/ref
-               :db/cardinality :db.cardinality/many
+    :entities {:db/cardinality :db.cardinality/many
+               :db/valueType   :db.type/ref
                :db/isComponent true}})
 
 (def Actor
@@ -65,23 +67,67 @@
 (defn q-last-moment [timeline]
   (q-moment timeline (q-last-epoch timeline)))
 
-(defn make-new-entity [m]
+(defn- make-new-entity [m]
   (prewalk (fn [node]
              (if (map? node)
-               (if (= '(:db/id) (keys node))
-                 (str "id-" (->> node vals first))
-                 (update node :db/id #(str "id-" %)))
+               (let [map-some    (fn [[k v]] (when v [k v]))
+                     nil-removed (into {} (map map-some node))
+                     new-entity  (->> nil-removed
+                                      (sp/transform [:db/id some?] #(str "id-" %))
+                                      (sp/setval [:db/id nil?] sp/NONE))]
+                 new-entity)
                node)) m))
 
 ;; specter paths
 (defn entity [actor-name]
   [:moment.attr/entities sp/ALL #(= actor-name (:actor.attr/name %))])
 
-(defn effect-on [actor-name effect-name]
-  [:moment.attr/entities sp/ALL #(= actor-name (:actor.attr/name %))
-   :actor.attr/effects sp/ALL #(= effect-name (:effect.attr/effect-name %))])
+(defn entity-id [actor-id]
+  [:moment.attr/entities sp/ALL #(= actor-id (:db/id %))])
+
+(defn on-effect [effect-name]
+  [:actor.attr/effects sp/ALL #(= effect-name (:effect.attr/effect-name %))])
+
 
 ;; timeline core
+
+
+(defn unleash-rules! [timeline unleashed-rules]
+  (loop [moment                       (-> (q-last-moment timeline)
+                                          (assoc :moment.attr/events []))
+         [unleashed-rule & remaining] unleashed-rules]
+    (let [[activated rule] unleashed-rule
+          rule-fn          (:rule/rule-fn rule)
+          new-moment       (rule-fn moment activated)]
+      (prn ["unleashing!!!" rule new-moment])
+      (tap> new-moment)
+      (if (some? remaining)
+        (recur new-moment remaining)
+        (do (prn ["transacting!" (make-new-entity new-moment)])
+            (d/transact! timeline [(make-new-entity (sp/transform [:moment.attr/epoch] inc new-moment))])
+            (prn ["done transacting!"]))))))
+
+(defn engrave-rules!
+  [timeline rules timing]
+  (loop [current-epoch (q-last-epoch timeline)
+         rules (filter #(= (:rule/timing %) timing) rules)]
+    (let [{:engrave!/keys [unleashed-rules dormant-rules]}
+          (->> rules
+               (mapv (fn [{:rule/keys [activation] :as rule}]
+                       (let [query-stmt (conj activation ['?current-moment :moment.attr/epoch current-epoch])
+                             activated  (d/q query-stmt (d/db timeline))]
+                         [activated rule])))
+               (group-by (fn [[activated _]]
+                           (if (some? activated)
+                             :engrave!/unleashed-rules
+                             :engrave!/dormant-rules))))]
+      (when (some? unleashed-rules)
+        (prn ["unleashed!" unleashed-rules dormant-rules])
+        (unleash-rules! timeline unleashed-rules))
+      (if (and (some? unleashed-rules) (some? dormant-rules))
+        (recur (inc current-epoch) dormant-rules)
+        :engrave!/done))))
+
 (defn engrave-action!
   [timeline moves {:action.attr/keys [actor act-expr] :as action}]
   (let [[move move-attr] act-expr
@@ -89,19 +135,26 @@
                              (assoc :move.attr/actor actor)
                              (assoc :move.attr/move-name move))
         alter            (get moves move)
-        moment           (make-new-entity (q-last-moment timeline))
+        moment           (q-last-moment timeline)
+        _ (prn ["moment of " (:moment.attr/epoch moment)])
         new-moment       (->> (alter moment move-attr)
                               (sp/transform [:moment.attr/epoch] inc)
                               (sp/setval    [:moment.attr/events]
                                             [(update action :action.attr/act-expr str)]))]
-    (d/transact! timeline [new-moment])))
+
+    (d/transact! timeline [(make-new-entity new-moment)])))
 
 (defn engrave! [timeline world history]
-  (let [{:world/keys [moves effects initial]} world
+  (let [{:world/keys [moves rules initial]} world
         moves (-> moves (update-vals eval))]
     (d/transact! timeline [initial])
     (loop [[action & remaining-actions] history]
+      (prn "before-action!")
+      (engrave-rules! timeline rules :timing/before-action)
+      (prn "action!")
       (engrave-action! timeline moves action)
+      (prn "after-action!")
+      (engrave-rules! timeline rules :timing/after-action)
       (if (empty? remaining-actions)
         ["last-moment" (q-last-moment timeline)]
         (recur remaining-actions)))))
--- src/model/topaz.clj
+++ src/model/topaz.clj
@@ -1,6 +1,6 @@
 (ns model.topaz
   (:require [com.rpl.specter :as sp]
-            [engine2.timeline :refer [effect-on entity]]))
+            [engine2.timeline :refer [entity entity-id on-effect]]))
 
 (defn basic-attack [moment {:move.attr/keys [actor target]}]
   (let [damage 50]
@@ -20,58 +20,56 @@
 (defn poison
   [moment {:move.attr/keys [actor target duration] :or {duration 3}}]
   (let [mp-cost 30 effect-name :debuff/poison
-        actor-entity (sp/select-one [(entity :char/hilda)] moment)]
+        actor-entity (sp/select-one [(entity actor)] moment)]
     (->> moment
          (sp/setval    [:moment.attr/desc] (str actor " poisons " target " ! " target " is now poisoned!"))
          (sp/transform [(entity actor)]
                        #(update % :actor.attr/mp - mp-cost))
-         (sp/setval    [(effect-on target :debuff/poison)]
+         (sp/setval    [(entity target) (on-effect :debuff/poison)]
                        #:effect.attr{:effect-name effect-name
                                      :source (:db/id actor-entity)
                                      :duration duration}))))
 
 (def debuff-poison
   #:rule
-   {:rule-name       :debuff/poison
-    :activation
-    '[:find [?affected ?source]
-      :where
-      [:info/moment :moment/event :event/on-moment-begins]
-      [:info/moment :moment/whose ?affected]
-      [?affected :attr/effects ?eff-id]
-      [?eff-id :effect-data/effect-name :debuff/poison]
-      [?eff-id :effect-data/source ?source]
-      [?eff-id :effect-data/duration ?duration]]
-    :rule-fn
-    (fn [moment [?affected ?source]]
-      (let [affected-entity (sp/select-one [(entity ?affected)] moment)
-            source-entity   (sp/select-one [(entity ?source)] moment)
-            affected-hp     (:attr/hp affected-entity)
-            damage          (Math/floor (/ affected-hp 10))]
-        (-> moment
-            (sp/transform [(entity ?affected)]
-                          #(update % :actor.attr/hp - damage))
-            (sp/setval    [:moment.attr/desc] (str ?affected " is poisoned! receives " damage " damage!"))
-            (sp/transform [(effect-on ?affected :debuff/poison) :effect-data/duration] dec)
-            (sp/setval    [:moment.attr/events]
-                          [#:event.attr{:event-name :debuff/poison
-                                        :damage      damage
-                                        :affected   (:db/id source-entity)
-                                        :source     (:db/id source-entity)}]))))})
+   {:rule-name  :debuff/poison
+    :timing     :timing/before-action
+    :activation '[:find [?affected-id ?source-id]
+                  :where
+                  [?current-moment :moment.attr/entities ?affected-id]
+                  [?affected-id :actor.attr/effects ?eff-id]
+                  [?eff-id :effect.attr/effect-name :debuff/poison]
+                  [?eff-id :effect.attr/source ?source-id]]
+    :rule-fn    (fn [moment [?affected-id ?source-id]]
+                  (let [affected-entity (sp/select-one [(entity-id ?affected-id)] moment)
+                        source-entity   (sp/select-one [(entity-id ?source-id)] moment)
+                        affected-hp     (:actor.attr/hp affected-entity)
+                        damage          (Math/floor (/ affected-hp 10))]
+                    (->> moment
+                         (sp/transform [(entity-id ?affected-id)]
+                                       #(update % :actor.attr/hp - damage))
+                         (sp/setval    [:moment.attr/desc] (str (:actor.attr/name affected-entity) " is poisoned! receives " damage " damage!"))
+                         (sp/transform [(entity-id ?affected-id) (on-effect :debuff/poison) :effect.attr/duration] dec)
+                         (sp/transform [:moment.attr/events]
+                                       #(conj % #:event.attr{:event-name  :debuff/poison
+                                                             :damage      damage
+                                                             :affected-id (select-keys affected-entity [:db/id])
+                                                             :source-id   (select-keys source-entity [:db/id])})))))})
 
 (def world
   #:world
    {:moves   {:move/basic-attack 'model.topaz/basic-attack
               :move/fireball     'model.topaz/fireball
               :move/poison       'model.topaz/poison}
-    :effects [debuff-poison]
+    :rules   [debuff-poison]
     :initial #:moment.attr
               {:epoch    0
                :entities [#:actor.attr
                            {:db/id "hilda"
                             :name  :char/hilda
                             :hp    700
-                            :mp    400}
+                            :mp    400
+                            :effects []}
                           #:actor.attr
                            {:name    :char/aluxes
                             :hp      1000
