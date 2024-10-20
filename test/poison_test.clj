ba9ad8bb8a70607c3239e7247297c627a79bb14d
keychera <kyogatama@gmail.com>
2024-10-21 00:20:53 +0900

    prep-test for both-poison

--- test/poison_test.clj
+++ test/poison_test.clj
@@ -8,7 +8,7 @@
             [util.test-data :refer [default-initial-moment with-fresh-timeline
                                     with-timeline]]))
 
-(def test-one-poison-world
+(def poison-world
   #:world
    {:actions {:move/basic-attack 'model.topaz/basic-attack
               :move/fireball     'model.topaz/fireball
@@ -16,7 +16,7 @@
     :rules   [topaz/debuff-poison]
     :initial default-initial-moment})
 
-(def history
+(def one-poison-history
   [#:action.attr{:action-name :move/poison
                  :actor-name  :char/hilda
                  :target-name :char/aluxes
@@ -29,19 +29,19 @@
                  :actor-name  :char/hilda
                  :target-name :char/aluxes}
    #:action.attr{:action-name :move/basic-attack
-                 :actor-name  :char/hilda
-                 :target-name :char/aluxes}
-
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}
+   
    #:action.attr{:action-name :move/fireball
                  :actor-name  :char/hilda
                  :target-name :char/aluxes}
    #:action.attr{:action-name :move/basic-attack
-                 :actor-name  :char/hilda
-                 :target-name :char/aluxes}])
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}])
 
 (def one-poison-timeline
   (with-fresh-timeline
-    (fn [timeline] (t/engrave! timeline test-one-poison-world history))))
+    (fn [timeline] (t/engrave! timeline poison-world one-poison-history))))
 
 (deftest count-gen-timeline
   (with-timeline one-poison-timeline
@@ -75,44 +75,77 @@
           (is :debuff/poison
               (sp/select-one [:moment.attr/facts sp/ALL :fact/effect-name some?] poison-moment)))))))
 
-(comment
-  (def test-both-poison-data
-    (build-history
-     [:char/aluxes :char/hilda]
-     [hilda/poison-effect]
-     [#:moment{:whose  :char/hilda
-               :action '(-> :char/hilda (poison :char/aluxes #:effect-data{:duration 1}))}
-      #:moment{:whose  :char/aluxes
-               :action '(-> :char/aluxes (poison :char/hilda #:effect-data{:duration 1}))}
+(def both-poison-history
+  [#:action.attr{:action-name :move/poison
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes
+                 :duration    2}
+   #:action.attr{:action-name :move/poison
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda
+                 :duration    1}
 
-      #:moment{:whose  :char/hilda
-               :action '(-> :char/hilda (magic-up))}
-      #:moment{:whose  :char/aluxes
-               :action '(-> :char/aluxes (basic-attack :char/hilda))}
+   #:action.attr{:action-name :move/fireball
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes}
+   #:action.attr{:action-name :move/basic-attack
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}
 
-      #:moment{:whose  :char/hilda
-               :action '(-> :char/hilda (magic-up))}
-      #:moment{:whose  :char/aluxes
-               :action '(-> :char/aluxes (basic-attack :char/hilda))}]))
+   #:action.attr{:action-name :move/fireball
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes}
+   #:action.attr{:action-name :move/basic-attack
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}])
 
-  (deftest test-both-poison
-    (let [actual-timeline (reduce-timeline 'model.hilda default-initial-moment test-both-poison-data 3)]
-      (is (= 9 (count actual-timeline)))
-      (is (= 0 (get-attr (first actual-timeline) :info/timeline :timeline/turn)))
-      (is (= 1 (get-attr (nth actual-timeline 1) :info/timeline :timeline/turn)))
-      (let [poison-moment (nth actual-timeline 2)]
-        (is (str/includes? (get-attr poison-moment :info/moment :moment/desc) "poison"))
-        (is (= :debuff/poison (get-attr poison-moment :info/moment :moment/effect-name)))
-        (is (= :char/aluxes (get-attr poison-moment :info/moment :moment/affected))))
-      (is (= 1 (get-attr (nth actual-timeline 3) :info/timeline :timeline/turn)))
-      (let [poison-moment (nth actual-timeline 4)]
-        (is (str/includes? (get-attr poison-moment :info/moment :moment/desc) "poison"))
-        (is (= :debuff/poison (get-attr poison-moment :info/moment :moment/effect-name)))
-        (is (= :char/hilda (get-attr poison-moment :info/moment :moment/affected))))
-      (is (= 2 (get-attr (nth actual-timeline 5) :info/timeline :timeline/turn)))
-      (is (= 2 (get-attr (nth actual-timeline 6) :info/timeline :timeline/turn)))
-      (is (= 3 (get-attr (nth actual-timeline 7) :info/timeline :timeline/turn)))
-      (is (= 3 (get-attr (nth actual-timeline 8) :info/timeline :timeline/turn)))))
+(def both-poison-timeline
+  (with-fresh-timeline
+    (fn [timeline] (t/engrave! timeline poison-world both-poison-history))))
 
+(deftest count-gen-both-poison-timeline
+  (with-timeline both-poison-timeline
+    (fn [timeline]
+      (let [epoch-count (d/q '[:find (count ?epoch) . :where [_ :moment.attr/epoch ?epoch]]
+                             (d/db timeline))]
+        (is (= 10 epoch-count))))))
 
-  (reduce-timeline 'model.hilda default-initial-moment test-poison-data 2))
+(deftest test-last-epoch-both-poison-timeline
+  (with-timeline both-poison-timeline
+    (fn [timeline]
+      (is (= 9 (t/q-last-epoch timeline))))))
+
+(deftest test-both-poison-interaction
+  (testing "poison interactions"
+    (with-timeline both-poison-timeline
+      (fn [timeline]
+        (let [poison-moment (t/q-moment timeline 2)]
+          (is (str/includes? (sp/select-one [:moment.attr/facts sp/ALL :fact/desc some?] poison-moment)
+                             "poisoned"))
+          (is (= :event/poison-unleashed
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/event some?] poison-moment)))
+          (is (= :debuff/poison
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/effect-name some?] poison-moment)))
+          (is (= :char/hilda
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/source-name some?] poison-moment))))
+        (let [poison-moment (t/q-moment timeline 4)]
+          (is (str/includes? (sp/select-one [:moment.attr/facts sp/ALL :fact/desc some?] poison-moment)
+                             "poisoned"))
+          (is (= :event/poison-unleashed
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/event some?] poison-moment)))
+          (is (= :debuff/poison
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/effect-name some?] poison-moment)))
+          (is (= :char/aluxes
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/source-name some?] poison-moment))))
+        (let [poison-moment (t/q-moment timeline 6)]
+          (is (str/includes? (sp/select-one [:moment.attr/facts sp/ALL :fact/desc some?] poison-moment)
+                             "poisoned"))
+          (is (= :event/poison-unleashed
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/event some?] poison-moment)))
+          (is (= :debuff/poison
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/effect-name some?] poison-moment)))
+          (is (= :char/aluxes
+                 (sp/select-one [:moment.attr/facts sp/ALL :fact/source-name some?] poison-moment))))
+        (let [poison-moment (t/q-moment timeline 8)]
+          (is (= :move/basic-attack
+                 (sp/select-one [:moment.attr/facts sp/ALL :action.attr/action-name some?] poison-moment))))))))
