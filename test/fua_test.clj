96d1203cdf6fb72c27da8e48dd8f62eab1c5abcd
keychera <kyogatama@gmail.com>
2024-10-21 21:07:19 +0900

    migrate fua test

--- test/fua_test.clj
+++ test/fua_test.clj
@@ -1,63 +1,51 @@
 (ns fua-test
   (:require [clojure.test :refer [deftest is]]
-            [engine.timeline :refer [reduce-timeline]]
-            [util.test-data :refer [default-initial-moment build-history]]))
+            [com.rpl.specter :as sp]
+            [engine2.timeline :as t :refer [entity]]
+            [model.topaz :as topaz]
+            [pod.huahaiy.datalevin :as d]
+            [util.test-data :refer [default-initial-moment with-fresh-timeline
+                                    with-timeline]]))
 
-(def clara-talent
-  [[:actor/aluxes :attr/effects 1]
-   [1 :effect-data/effect-name :talent/clara]])
+(def clara-world
+  #:world
+   {:actions {:move/basic-attack 'model.topaz/basic-attack
+              :move/fireball     'model.topaz/fireball}
+    :rules   [topaz/talent-clara]
+    :initial (->> default-initial-moment
+                  (sp/transform [(entity :char/aluxes) :actor.attr/effects]
+                                #(conj % {:effect.attr/effect-name :talent/clara})))})
 
-(def clara-initial-moment
-  (into default-initial-moment clara-talent))
+(def clara-history
+  [#:action.attr{:action-name :move/fireball
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes}
+   #:action.attr{:action-name :move/basic-attack
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}
 
-(def blade-talent
-  [[:actor/aluxes :attr/effects 2]
-   [2 :effect-data/effect-name :talent/blade]
-   [2 :effect-data/max-charge 2]
-   [2 :effect-data/charge 0]])
+   #:action.attr{:action-name :move/fireball
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes}
+   #:action.attr{:action-name :move/basic-attack
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}
 
-(def blade-initial-moment
-  (into default-initial-moment blade-talent))
+   #:action.attr{:action-name :move/fireball
+                 :actor-name  :char/hilda
+                 :target-name :char/aluxes}
+   #:action.attr{:action-name :move/basic-attack
+                 :actor-name  :char/aluxes
+                 :target-name :char/hilda}])
 
-(def clara-blade-initial-moment
-  (-> default-initial-moment (into clara-talent) (into blade-talent)))
+(def clara-timeline
+  (with-fresh-timeline
+    (fn [timeline] (t/engrave! timeline clara-world clara-history))))
 
-(def test-fua-history
-  (build-history
-   [:actor/aluxes :actor/hilda]
-   []
-   [#:moment{:whose  :actor/hilda
-             :action '(-> :actor/hilda (fireball :actor/aluxes))}
-    #:moment{:whose  :actor/aluxes
-             :action '(-> :actor/aluxes (basic-attack :actor/hilda))}
-    #:moment{:whose  :actor/hilda
-             :action '(-> :actor/hilda (fireball :actor/aluxes))}
-    #:moment{:whose  :actor/aluxes
-             :action '(-> :actor/aluxes (basic-attack :actor/hilda))}
-    #:moment{:whose  :actor/hilda
-             :action '(-> :actor/hilda (fireball :actor/aluxes))}
-    #:moment{:whose  :actor/aluxes
-             :action '(-> :actor/aluxes (basic-attack :actor/hilda))}]))
 
 (deftest test-clara-talent
-  (let [actual-timeline (reduce-timeline 'model.hilda clara-initial-moment test-fua-history 3)]
-    (is (= 10 (count actual-timeline)))))
-
-(deftest test-blade-talent
-  (let [actual-timeline (reduce-timeline 'model.hilda blade-initial-moment test-fua-history 3)]
-    (is (= 8 (count actual-timeline)))))
-
-(deftest test-clara-blade-talent
-  (let [actual-timeline (reduce-timeline 'model.hilda clara-blade-initial-moment test-fua-history 3)]
-    (is (= 11 (count actual-timeline)))))
-
-(comment
-  (require '[babashka.pods :as pods])
-  (pods/load-pod 'huahaiy/datalevin "0.9.10")
-  (require '[pod.huahaiy.datalevin :as d])
-
-  (->> (d/q '[:find ?talent-name
-              :where
-              [:actor/aluxes :attr/effects ?eid]
-              [?eid :effect-data/talent-name ?talent-name]]
-            clara-initial-moment)))
+  (with-timeline clara-timeline
+    (fn [timeline]
+      (let [epoch-count (d/q '[:find (count ?epoch) . :where [_ :moment.attr/epoch ?epoch]]
+                             (d/db timeline))]
+        (is (= 10 epoch-count))))))
